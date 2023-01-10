f0e713c3dfbbb699af6634a73edac3982e87eaf4
coder-ms <marco.sbrascia@gmail.com>
2023-01-10 23:31:53 +0200

    add style

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -1,9 +1,19 @@
 @extends('layouts.app')
 
 @section('content')
-    <ul>
+
+    <div class="containerx">
+
         @foreach ($posts as $post)
-            <li>{{$post->title}}</li>
+            <div class="card">
+                <div class="cardDescription"> 
+                    <p>Nome Repo: {{$post->title}}</p>
+                    <p>Modello: {{$post->slug}}</p>
+                    <p>Descrizione: <br>{{ Str::limit($post->content)}}</p>
+                   
+                </div>
+            </div>
+        
         @endforeach
-    </ul>
+    </div>
 @endsection
