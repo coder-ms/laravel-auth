2d4756daad9014dc587c3bba9c6a29014aa7ae94
coder-ms <marco.sbrascia@gmail.com>
2023-01-10 22:55:40 +0200

    Base CRUD

---
+++ resources/views/admin/posts/edit.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.app')
+
+@section('content')
+    <h1>EDIT POST</h1>
+@endsection
---
+++ resources/views/admin/posts/show.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.app')
+
+@section('content')
+    <h1>SHOW POST</h1>
+@endsection
