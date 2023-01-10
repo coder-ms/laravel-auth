ce8c97c2bed0f3f202c62430f7270f3da5808311
coder-ms <marco.sbrascia@gmail.com>
2023-01-10 19:36:24 +0200

    completed seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,5 +20,10 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        //Lancio tutti i seeder in contemporanea
+        $this->call([
+            PostTableSeeder::class,
+        ]);
     }
 }
